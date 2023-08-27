dfab9153c884a445a7772f386a1ea0d84d403282
Fernando Rojo <frojo@sas.upenn.edu>
2023-08-27 17:17:01 -0400

    struct

--- ios/GaleriaView.swift
+++ ios/GaleriaView.swift
@@ -21,35 +21,4 @@ class GaleriaView: ExpoView {
     imageView.heightAnchor.constraint(equalToConstant: 100).isActive = true
   }
 }
-
-
-struct Data {
-    
-    static let imageNames:[String] = [
-        "cat1",
-        "cat2",
-        "cat3",
-        "cat4",
-        "cat5",
-        "cat1",
-        "cat2",
-        "cat3",
-        "cat4",
-        "cat5",
-        "cat1",
-        "cat2",
-        "cat3",
-        "cat4",
-        "cat5",
-        "cat1",
-        "cat2",
-        "cat3",
-        "cat4",
-        "cat5",
-    ]
-    
-    static let images:[UIImage] = Self.imageNames.compactMap { UIImage(named: $0)! }
-    
-    static let imageUrls:[URL] = Self.imageNames.compactMap {
-        URL(string: "https://raw.githubusercontent.com/michaelhenry/MHFacebookImageViewer/master/Example/Demo/Assets.xcassets/\($0).imageset/\($0).jpg")! }
-}
+ 
