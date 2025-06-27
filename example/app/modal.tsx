90a9ae1a828e2eedf398ad11014079580ea9d11c
Patrik <22777727+fedorish@users.noreply.github.com>
2025-06-27 20:41:55 +0200

    feat: add onindexchange

--- example/app/modal.tsx
+++ example/app/modal.tsx
@@ -1,8 +1,7 @@
-import { StatusBar } from 'expo-status-bar'
-import { Galeria } from 'galeria'
-import { Platform, StyleSheet, Text, View } from 'react-native'
-import { urls } from '../constants/Images'
 import { Image } from 'expo-image'
+import { Galeria } from 'galeria'
+import { StyleSheet, View } from 'react-native'
+import { urls } from '../constants/Images'
 
 export default function ModalScreen() {
   return (
